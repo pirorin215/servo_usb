28cefb62c81ef1e3fb69423892ad4ee7d4b63c01
pirorin215 <hogehoge215@gmail.com>
2026-03-01 12:23:55 +0900

    README.md作成

---
+++ monitor.sh
@@ -0,0 +1,1 @@
+arduino-cli monitor -p /dev/cu.usbmodem8
--- upload.sh
+++ upload.sh
@@ -1,1 +1,1 @@
-arduino-cli upload -p /dev/cu.usbmodem212201 -b arduino:avr:leonardo servo_usb.ino
+arduino-cli upload -p /dev/cu.usbmodem8 -b arduino:avr:leonardo servo_usb.ino
